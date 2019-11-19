2ec88428c62e8428025ea52113b81212a0b6da6a
Pengyin Chen <chen6636@umn.edu>
2019-11-18 19:46:58 -0600

    add

---
+++ multiple_processing_map_reduce/include/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <time.h>
+#include "phase1.h"
+#include "phase2.h"
+#include "phase3.h"
+#include "phase4.h"
+
+#endif
