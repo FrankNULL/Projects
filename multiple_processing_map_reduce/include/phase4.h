c1e94b59d4920b93588a3eb3fc5a16d580608120
Pengyin Chen <chen6636@umn.edu>
2019-11-18 19:47:56 -0600

    add

---
+++ multiple_processing_map_reduce/include/phase4.h
@@ -0,0 +1,19 @@
+// Add Guard to header file
+// Function prototypes for
+//			Master process's reporting procedure
+#ifndef PHASE4_H
+#define PHASE4_H
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+
+
+int exe_phase4();
+
+#endif
